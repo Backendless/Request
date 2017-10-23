76d89f2646730642fa43a56383aa377a94ac5607
Vladmir Upirov <vladimir.upirov@themidnightcoders.com>
2017-10-23 10:04:32 +0200

    - fix overriding XmlHttpRequest - upgrade Webpack to v3.8.1 - add declaration umd/module to package.json - optimize for using in web browser env

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,53 @@
 
 const webpack = require('webpack')
 const isProd = process.env.NODE_ENV === 'production'
-
 const uglify = new webpack.optimize.UglifyJsPlugin({
   compressor: {
     pure_getters: true,
-    unsafe: true,
+    unsafe      : true,
     unsafe_comps: true,
-    warnings: false,
+    warnings    : false,
+    screw_ie8   : false
+  },
+  mangle    : {
     screw_ie8: false
   },
-  mangle: {
+  output    : {
     screw_ie8: false
   },
-  output: {
-    screw_ie8: false
-  }
+  sourceMap : true
 })
 
+function ignoreRequiresFor(packages) {
+  return function(context, moduleName, callback) {
+    if (packages.includes(moduleName)) {
+      return callback(null, `commonjs ${moduleName}`);
+    }
+
+    callback();
+  }
+}
+
 module.exports = {
-  target: 'node',
+
+  devtool: 'source-map',
+
+  target: 'web',
+
+  externals: [ignoreRequiresFor(['url', 'http', 'https', 'stream', 'form-data', 'buffer'])],
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test   : /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader : 'babel-loader'
       }
     ]
   },
 
   output: {
-    library: 'BackendlessRequest',
+    library      : 'BackendlessRequest',
     libraryTarget: 'umd'
   },
 
