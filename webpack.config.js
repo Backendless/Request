68ed38a895f2ecd0c9fa7865aab8d7cad5fb808e
Vladmir Upirov <vladimir.upirov@themidnightcoders.com>
2017-11-10 12:17:13 +0100

    - cut out require node modules from "umd" build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,29 @@
 'use strict';
 
+const ExternalModule = require('webpack/lib/ExternalModule')
 const webpack = require('webpack')
+
+const nativeExternalModuleGetSourceForCommonJsExternal = ExternalModule.prototype.getSourceForCommonJsExternal
+
+const externalNodeModules = ['url', 'http', 'https', 'stream', 'form-data', 'buffer']
+
+ExternalModule.prototype.getSourceForCommonJsExternal = function(moduleAndSpecifiers) {
+  if (typeof moduleAndSpecifiers === 'string' && externalNodeModules.includes(moduleAndSpecifiers)) {
+    //we must not include external NodeJs packages to "dist" build by several reasons,
+    //one of these is problem with ReactNative http://support.backendless.com/topic/latest-npm-version-4-1-6-not-working-with-react-native
+    return [
+      'throw new Error(\'',
+      `NodeJs package "${moduleAndSpecifiers}" is not included to "dist" build. `,
+      'Do not use the file in NodeJs environment. ',
+      'You should use files from "lib" directory instead. ',
+      'If you have some problems with it or any questions please create a new support topic here http://support.backendless.com/ ',
+      '\')'
+    ].join('');
+  }
+
+  return nativeExternalModuleGetSourceForCommonJsExternal.apply(this, arguments)
+}
+
 const isProd = process.env.NODE_ENV === 'production'
 const uglify = new webpack.optimize.UglifyJsPlugin({
   compressor: {
@@ -19,23 +42,19 @@ const uglify = new webpack.optimize.UglifyJsPlugin({
   sourceMap : true
 })
 
-function ignoreRequiresFor(packages) {
-  return function(context, moduleName, callback) {
-    if (packages.includes(moduleName)) {
-      return callback(null, `commonjs ${moduleName}`);
-    }
-
-    callback();
-  }
-}
-
 module.exports = {
 
   devtool: 'source-map',
 
   target: 'web',
 
-  externals: [ignoreRequiresFor(['url', 'http', 'https', 'stream', 'form-data', 'buffer'])],
+  externals: function(context, moduleName, callback) {
+    if (externalNodeModules.includes(moduleName)) {
+      return callback(null, `commonjs ${moduleName}`);
+    }
+
+    callback();
+  },
 
   module: {
     rules: [
